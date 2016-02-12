917392e41d6629b78d05c32b285a39e2000b2a75
Allan Arnesen <allan.arnesen@gmail.com>
2016-02-12 17:31:48 +0100

    Ferdigstilt Versjon_0.2

--- Oblig_2_V2016/src/KortTest.java
+++ Oblig_2_V2016/src/KortTest.java
@@ -7,31 +7,13 @@ public class KortTest {
 		Kort k2 = new Gjest("Marit Olsen", 9999);
 		reg.add(k1);
 		reg.add(k2);
-		// add-on
-		Scanner input = new Scanner(System.in);
-		while (true){
-			   System.out.println("Main Menu:");
-			   System.out.println("1: Run program check");
-			   int choice = input.nextInt();
-			   switch (choice){
-			   case 1: 
-				// add-on
-				   // save - beg
-				   for(int i = 0; i < reg.size(); i++){
-					Kort kort = (Kort)reg.get(i);
-					System.out.println(kort);
-					System.out.println("Test av kort: " + kort.getKortNummer() + ",med kode 1234 er: " + (kort.sjekkPIN(1234) ? " Gyldig" : " Ugyldig"));
-					System.out.println("Test av kort: " + kort.getKortNummer() + ",med kode 9999 er: " + (kort.sjekkPIN(9999) ? " Gyldig" : " Ugyldig"));
-					System.out.println("-------------------------------------------------");
-					// save - end
-				   }
-				// add-on
-					break;
-			   default: System.out.println("That choice doesnt exsist.\nPlease try again.\n\n\n"); 
-			    break;
-			   }
-			// add-on
+
+		for(int i = 0; i < reg.size(); i++){
+			Kort kort = (Kort)reg.get(i);
+			System.out.println(kort);
+			System.out.println("Test av kort: " + kort.getKortNummer() + ",med kode 1234 er: " + (kort.sjekkPIN(1234) ? " Gyldig" : " Ugyldig"));
+			System.out.println("Test av kort: " + kort.getKortNummer() + ",med kode 9999 er: " + (kort.sjekkPIN(9999) ? " Gyldig" : " Ugyldig"));
+			System.out.println("-------------------------------------------------");
 		}
 	}
-
 }
